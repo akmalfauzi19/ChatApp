3a3cbe8ce960839cfcedfd15a54082adae740d4f
akmal fauzi <akmal.fauzi19@gmail.com>
2021-06-23 21:07:11 +0700

    add view chat app

---
+++ resources/views/livewire/messages.blade.php
@@ -0,0 +1,44 @@
+<div>
+    <div class="row justify-content-center">
+
+        {{-- START col-md_4 --}}
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Users
+                </div>
+                <div class="card-body chatbox p-0">
+                    <ul>
+                        <a href="">
+                            <li class="list-item">HAFIZH</li>
+                        </a>
+                        <a href="">
+                            <li class="list-item">HAFIZH</li>
+                        </a>
+                        <a href="">
+                            <li class="list-item">HAFIZH</li>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        {{-- END col-md_4 --}}
+
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    Hafizh
+                </div>
+                <div class="card-body">
+                    MESAGE
+                </div>
+                <div class="card-footer">
+                    <form action="">
+                        <input type="text" wire:model="message">
+                        <button class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
