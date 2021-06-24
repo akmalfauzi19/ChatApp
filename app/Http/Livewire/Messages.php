662425b795aed9f8a1c45e87977e2cc6baaf0804
akmal fauzi <akmal.fauzi19@gmail.com>
2021-06-24 21:05:17 +0700

    update controller livewire Message

--- app/Http/Livewire/Messages.php
+++ app/Http/Livewire/Messages.php
@@ -2,22 +2,82 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Message;
 use App\Models\User;
+use GuzzleHttp\Psr7\Request;
 use Livewire\Component;
-
-
+use Illuminate\Support\Facades\Auth;
 
 class Messages extends Component
 {
+    public $sender;
+    public $message;
+    public $allMessage;
+
     public function render()
     {
-        $users = User::all();
-        return view('livewire.messages', compact('users'));
+        $model = new User();
+        $users = $model->all();
+        $sender = $this->sender;
+        // $this->allMessage = Message::all();
+        // $this->allMessage;
+
+        return view('livewire.messages', compact('users', 'sender'));
     }
 
-    public function getUser($user)
+    public function resetForm()
+    {
+        $this->message = '';
+    }
+
+    public function mountData()
+    {
+        $model = new Message;
+
+        if (isset($this->sender->id)) {
+
+            $this->allMessage = $model->where('user_id', Auth::user()->id)
+                ->where('receiver_id', $this->sender->id)
+                ->orWhere('user_id', $this->sender->id)
+                ->where('receiver_id', Auth::user()->id)
+                ->orderBy('id', 'desc')->get();
+        }
+
+        if (isset($this->sender->id)) {
+            // for update unread messages
+            $not_seen = $model->where('user_id', $this->sender->id)
+                ->where('receiver_id', Auth::user()->id)->where('is_seen', false);
+
+            $not_seen->update(['is_seen' => true]);
+        }
+    }
+
+    public function getUser($user_id)
+    {
+        $model = new User();
+        $user = $model->findOrfail($user_id);
+        $this->sender = $user;
+        // $this->allMessage = Message::all();
+        $this->allMessage = Message::where('user_id', Auth::user()->id)
+            ->where('receiver_id', $user_id)
+            ->orWhere('user_id', $user_id)
+            ->where('receiver_id', Auth::user()->id)
+            ->orderBy('id', 'desc')->get();
+
+
+        // dd($this->sender);
+    }
+
+    public function SendMessage()
     {
         //
-        dd($user);
+        $model = new Message();
+
+        $model->message = $this->message;
+        $model->user_id = Auth::user()->id;
+        $model->receiver_id = $this->sender->id;
+        $model->save();
+
+        $this->resetForm();
     }
 }
