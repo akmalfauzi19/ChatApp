bd4d4e258d6c873aa9c5fd6974ec341dab4b0f2a
akmal fauzi <akmal.fauzi19@gmail.com>
2021-06-25 16:46:20 +0700

    update route name

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::view('chat', 'users.messages');
+Route::view('chat', 'users.messages')->name('chat');
